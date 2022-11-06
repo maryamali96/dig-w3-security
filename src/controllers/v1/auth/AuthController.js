48700433f39d3266739984aa50040c9348fc766d
maryam ali altimimi <maryamana515@yahoo.com>
2022-11-06 09:49:42 +0300

    ready for staging

--- src/controllers/v1/auth/AuthController.js
+++ src/controllers/v1/auth/AuthController.js
@@ -4,7 +4,6 @@ let jwt = require('jsonwebtoken')
 let bcrypt = require('bcryptjs')
 require('dotenv').config()
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
-console.log("JWT_SECRET_KEY="+JWT_SECRET_KEY);
 const {success, error} = require('../../../utils/responser')
 const {validationResult} = require('express-validator')
 
@@ -64,7 +63,7 @@ const signup = (req, res)=>{
 
 const signin = async (req, res)=>{
     let errors = validationResult(req).array();
-    if(errors)
+    if(errors.length> 0)
     {
         return res.status(400).json(error(400,errors))
 
